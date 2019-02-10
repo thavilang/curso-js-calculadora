9eae10cab0ba98579597965632341865051ae71b
Thavi Lang <thavilang@gmail.com>
2019-02-10 18:50:37 -0200

    esctrutura base

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Calculadora Hcode</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="outer">
+        <div class="middle">
+            <?php @include 'calculadora.svg'; ?>
+        </div>
+    </div>
+</body>
+</html>
